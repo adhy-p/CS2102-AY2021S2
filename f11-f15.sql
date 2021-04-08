30656748a67477cefa5cfbc218b0f49738a502f7
jusufnathanael <jusuf.n@gmail.com>
2021-04-08 21:13:00 +0800

    Fixed bugs f11-f15

--- f11-f15.sql
+++ f11-f15.sql
@@ -1,32 +1,49 @@
-CREATE OR REPLACE FUNCTION add_course_package(
-    package_name VARCHAR, no_of_free_sessions INTEGER, package_start_date DATE, 
-    package_end_date DATE, package_price DOUBLE
-    ) AS $$
-    INSERT INTO Course_Packages (sale_start_date, sale_end_date, name, num_free_registration, price)
-    VALUES (package_start_date, package_end_date, package_name, no_of_free_sessions, package_price)
+DROP PROCEDURE IF EXISTS add_course_package;
+CREATE OR REPLACE PROCEDURE add_course_package(
+    package_name text, no_of_free_sessions INTEGER, package_start_date DATE, 
+    package_end_date DATE, package_price DOUBLE PRECISION
+) AS $$
+    INSERT INTO Course_Packages
+    VALUES (
+        (SELECT MAX(package_id) + 1 FROM Course_Packages),
+        package_start_date, package_end_date, package_name, no_of_free_sessions, package_price
+    )
 $$ LANGUAGE sql;
 
+
+DROP FUNCTION IF EXISTS get_available_course_packages();
 CREATE OR REPLACE FUNCTION get_available_course_packages()
-RETURNS RECORD AS $$
+RETURNS TABLE (
+    package_name VARCHAR(50), no_of_free_sessions INTEGER,
+    end_date DATE, price DOUBLE PRECISION
+) AS $$
     SELECT name, num_free_registration, sale_end_date, price
     FROM Course_Packages
 $$ LANGUAGE sql;
 
-CREATE OR REPLACE FUNCTION buy_course_package(
-    IN customer_id INTEGER, IN course_package_id INTEGER) AS $$
-IF EXISTS(SELECT * FROM Customers WHERE cust_id = customer_id) AND
-    EXISTS(SELECT * FROM Course_Packages WHERE package_id = course_package_id) AND
-    EXISTS(SELECT * FROM Credit_Cards WHERE cust_id = customer_id)
+
+DROP PROCEDURE IF EXISTS buy_course_package;
+CREATE OR REPLACE PROCEDURE buy_course_package(IN customer_id INTEGER,
+IN course_package_id INTEGER) AS $$
     BEGIN
+    IF EXISTS (
+        (SELECT * FROM Customers WHERE cust_id = customer_id) AND
+        EXISTS(SELECT * FROM Course_Packages WHERE package_id = course_package_id) AND
+        EXISTS(SELECT * FROM Credit_Cards WHERE cust_id = customer_id)
+    ) THEN
         INSERT INTO Buys VALUES (
-            DATETODAY, course_package_id,
-            SELECT card_number FROM Credit_Cards WHERE cust_id = customer_id, 
-            SELECT num_free_registration FROM Course_Packages WHERE package_id = course_package_id    
-        )
-    END
+            current_date, course_package_id,
+            (SELECT card_number FROM Credit_Cards WHERE cust_id = customer_id),
+            (SELECT num_free_registration FROM Course_Packages WHERE package_id = course_package_id)
+        );
+        END IF;
+    END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION get_my_course_package(customer_id INTEGER) AS $$
+
+DROP FUNCTION IF EXISTS get_my_course_package(INTEGER);
+CREATE OR REPLACE FUNCTION get_my_course_package(customer_id INTEGER)
+RETURNS json AS $$
     SELECT row_to_json(t)
     FROM (
         SELECT name, purchase_date, price, num_free_registration,
@@ -44,13 +61,21 @@ CREATE OR REPLACE FUNCTION get_my_course_package(customer_id INTEGER) AS $$
     ) t
 $$ LANGUAGE sql;
 
-CREATE OR REPLACE FUNCTION get_available_course_offerings() AS $$
+
+DROP FUNCTION IF EXISTS get_available_course_offerings();
+CREATE OR REPLACE FUNCTION get_available_course_offerings()
+RETURNS TABLE (
+    course_title VARCHAR(50), course_area VARCHAR(50), start_date DATE,
+    end_date DATE, registration_deadline DATE, course_fees DOUBLE PRECISION, 
+    remaining_seats BIGINT
+) AS $$
+
     SELECT A.title, A.name, A.start_date, A.end_date, A.registration_deadline, A.fees,
-    A.seating_capacity - count(R.course_id) AS no_of_remaining_redemption
+    A.seating_capacity - COUNT(R.course_id) AS remaining_seats
     FROM (Offerings NATURAL JOIN Courses) A LEFT JOIN Registers R
     ON A.launch_date = R.launch_date and A.course_id = R.course_id
     GROUP BY A.course_id, A.launch_date, A.title, A.name, A.start_date, A.end_date,
-    A.registration_deadline, A.fees
-    HAVING A.seating_capacity - count(R.course_id) > 0;
-$$ LANGUAGE sql;
+    A.registration_deadline, A.fees 
+    HAVING A.seating_capacity - COUNT(R.course_id) > 0; 
 
+$$ LANGUAGE sql;
