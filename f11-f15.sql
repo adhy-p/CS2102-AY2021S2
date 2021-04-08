7bc8bf1a88b353512b95556734eacdf639a54197
jusufnathanael <jusuf.n@gmail.com>
2021-04-08 20:04:42 +0800

    Done f11-f15

--- f11-f15.sql
+++ f11-f15.sql
@@ -2,10 +2,8 @@ CREATE OR REPLACE FUNCTION add_course_package(
     package_name VARCHAR, no_of_free_sessions INTEGER, package_start_date DATE, 
     package_end_date DATE, package_price DOUBLE
     ) AS $$
-
     INSERT INTO Course_Packages (sale_start_date, sale_end_date, name, num_free_registration, price)
     VALUES (package_start_date, package_end_date, package_name, no_of_free_sessions, package_price)
-
 $$ LANGUAGE sql;
 
 CREATE OR REPLACE FUNCTION get_available_course_packages()
@@ -47,11 +45,12 @@ CREATE OR REPLACE FUNCTION get_my_course_package(customer_id INTEGER) AS $$
 $$ LANGUAGE sql;
 
 CREATE OR REPLACE FUNCTION get_available_course_offerings() AS $$
-
-    SELECT title, name, start_date, end_date, registration_deadline, fees, ...
-    FROM Offerings NATURAL JOIN Courses ORDER BY registered_deadline, title
-
-    /* the number of remaining seats. */
-
+    SELECT A.title, A.name, A.start_date, A.end_date, A.registration_deadline, A.fees,
+    A.seating_capacity - count(R.course_id) AS no_of_remaining_redemption
+    FROM (Offerings NATURAL JOIN Courses) A LEFT JOIN Registers R
+    ON A.launch_date = R.launch_date and A.course_id = R.course_id
+    GROUP BY A.course_id, A.launch_date, A.title, A.name, A.start_date, A.end_date,
+    A.registration_deadline, A.fees
+    HAVING A.seating_capacity - count(R.course_id) > 0;
 $$ LANGUAGE sql;
 
